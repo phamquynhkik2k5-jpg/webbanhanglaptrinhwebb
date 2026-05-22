7843f3bdd445cb2d4cea76061cc1781191efbb7e
Nguyen Pham Quynh <phamquynhkik2k5@gmail.com>
2026-05-22 10:45:19 +0700

    Update product image upload and display

--- Webbanhang/Controllers/HomeController.cs
+++ Webbanhang/Controllers/HomeController.cs
@@ -1,21 +1,32 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Webbanhang.Models;
+using Webbanhang.Repositories;
 
 namespace Webbanhang.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductRepository _productRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IProductRepository productRepository)
         {
             _logger = logger;
+            _productRepository = productRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Lấy 6 sản phẩm mới nhất để hiển thị trên trang chủ
+            var products = _productRepository.GetAll()
+                .OrderByDescending(p => p.Id)
+                .Take(6)
+                .ToList();
+
+            return View(products);
         }
 
         public IActionResult Privacy()
